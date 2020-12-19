cc2766f8d88b30ea91e46e44727a19257fa2239f
Travis Delly <tdellytech@gmail.com>
2020-12-18 23:27:48 -0700

    change/slippage (#24)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  configDomain: 'assets/js',
-  freeClaimApi: 'https://api.axion.network',
-  slippageTolerancePercent: 0.5,
-  auctionRecipientPercent: 20
+  configDomain: "assets/js",
+  freeClaimApi: "https://api.axion.network",
+  slippageTolerancePercent: 1.5,
+  auctionRecipientPercent: 20,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: true,
-  configDomain: 'assets/js',
-  freeClaimApi: 'https://api.axion.network',
-  slippageTolerancePercent: 0.5,
-  auctionRecipientPercent: 20
+  configDomain: "assets/js",
+  freeClaimApi: "https://api.axion.network",
+  slippageTolerancePercent: 1.5,
+  auctionRecipientPercent: 20,
 };
 
 /*
