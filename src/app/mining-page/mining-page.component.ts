f3f06c6c509aefb54ece0ac6cf849ed63245586a
Jordan <jordan.travaux@hotmail.com>
2021-01-18 18:04:21 -0500

    removed unused code

--- src/app/mining-page/mining-page.component.ts
+++ src/app/mining-page/mining-page.component.ts
@@ -7,14 +7,10 @@ import {
   TemplateRef,
 } from "@angular/core";
 import BigNumber from "bignumber.js";
-import * as moment from "moment";
-import { CookieService } from "ngx-cookie-service";
 import { AppComponent } from "../app.component";
-import { AppConfig } from "../appconfig";
-import { MetamaskErrorComponent } from "../components/metamaskError/metamask-error.component";
-
-import { MiningContractService } from "../services/mining-contract";
 import { MatDialog } from "@angular/material/dialog";
+import { MiningContractService } from "../services/mining-contract";
+import { MetamaskErrorComponent } from "../components/metamaskError/metamask-error.component";
 
 @Component({
   selector: "app-mining-page",
@@ -29,65 +25,60 @@ export class MiningPageComponent implements OnDestroy {
   public pools: any[];
   public userTokenBalance: any = {};
   public currentPool: any = {};
+  public currentPoolStats: any = {};
   public depositAmount = 0;
-
   private accountSubscribe;
-  private settings: any = {};
 
   constructor(
     public contractService: MiningContractService,
-    private cookieService: CookieService,
     private ngZone: NgZone,
     private appComponent: AppComponent,
-    private config: AppConfig,
     private dialog: MatDialog
   ) {
-    this.accountSubscribe = this.contractService
-      .accountSubscribe()
-      .subscribe((account: any) => {
-        if (!account || account.balances) {
-          this.ngZone.run(() => {
-            this.account = account;
-            window.dispatchEvent(new Event("resize"));
+    this.accountSubscribe = this.contractService.accountSubscribe().subscribe((account: any) => {
+      if (!account || account.balances) {
+        this.ngZone.run(() => {
+          this.account = account;
+          window.dispatchEvent(new Event("resize"));
 
-            if (account) {
-              this.onChangeAccount.emit();
-              this.pools  = this.getPools();
-              this.currentPool = this.pools[0];
-              this.contractService.getDecimals(this.currentPool.address).then(decimals => this.tokensDecimals = decimals )
-              this.updatePoolBalances();    
-            }
-          });
-        }
-      });
-
-    this.settings = config.getConfig();
+          if (account) {
+            this.onChangeAccount.emit();
+            this.pools  = this.getPools();
+            this.currentPool = this.pools[0];
+            this.currentPoolStats = this.getPoolStats(account.address);
+            this.contractService.getDecimals(this.currentPool.address).then(decimals => this.tokensDecimals = decimals )
+            this.updatePoolBalances();    
+          }
+        });
+      }
+    });
   }
 
   ngOnDestroy() {
     this.accountSubscribe.unsubscribe();
   }
 
-  public subscribeAccount() {
-    this.appComponent.subscribeAccount();
-  }
-
-  // TODO: Get actual pools & account's data
+  // TODO: Get actual pools
   private getPools() {
     return [{
       address: "0xaadb00551312a3c2a8b46597a39ef1105afb2c08",
       base: "AXN",
       market: "ETH",
-
-      stakedTokens: new BigNumber(0),
-      rewardsEarned: new BigNumber(0),
-
-      isLive: true,
-      depositProgress: false,
-      withdrawLPProgress: false,
-      withdrawAllProgress: false,
-      withdrawRewardsProgress: false,
+      isLive: true
     }]
+  }
+
+  // TODO: Get actual account data
+  private getPoolStats(address) {
+    return {
+      stakedTokens: new BigNumber(0),
+      rewardsEarned: new BigNumber(0)
+    }
+  }
+
+
+  public subscribeAccount() {
+    this.appComponent.subscribeAccount();
   }
 
   public async depositLPTokens() {
@@ -97,18 +88,18 @@ export class MiningPageComponent implements OnDestroy {
       try {
         this.currentPool.depositProgress = true;
         await this.contractService[methodName](this.depositAmount);
-        this.contractService.updateETHBalance();
+        this.updatePoolBalances()
         this.getPools();
-      } catch (err) {
+      } 
+      catch (err) {
         if (err.message) {
           this.dialog.open(MetamaskErrorComponent, {
             width: "400px",
-            data: {
-              msg: err.message,
-            },
+            data: { msg: err.message },
           });
         }
-      } finally {
+      } 
+      finally {
         this.currentPool.depositProgress = false;
       }
     }
@@ -136,16 +127,16 @@ export class MiningPageComponent implements OnDestroy {
       // Update info after success
       this.updatePoolBalances();
       this.getPools();
-    } catch (err) {
+    } 
+    catch (err) {
       if (err.message) {
         this.dialog.open(MetamaskErrorComponent, {
           width: "400px",
-          data: {
-            msg: err.message,
-          },
+          data: { msg: err.message },
         });
       }
-    } finally {
+    } 
+    finally {
       this.currentPool[progressIndicatorVariable] = false;
     }
   }
--- src/app/services/mining-contract/index.ts
+++ src/app/services/mining-contract/index.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import BigNumber from "bignumber.js";
-import * as moment from "moment";
-import { Observable, Subscriber } from "rxjs";
-import { Contract } from "web3-eth-contract";
-import { environment } from "../../../environments/environment";
+import { Observable } from "rxjs";
 import { AppConfig } from "../../appconfig";
 import { ContractService } from "../contract";
 import { MetamaskService } from "../web3";
@@ -13,99 +9,21 @@ import { MetamaskService } from "../web3";
   providedIn: "root",
 })
 export class MiningContractService {
-  private web3Service;
-
-  private isActive: boolean;
-
-  private tokensDecimals: any = {
-    ETH: 18,
-  };
-
-  private readonly ETHER = Math.pow(10, this.tokensDecimals.ETH);
-
   public account;
+  private isActive: boolean;
+  public axnTokenAddress: string;
   private allAccountSubscribers = [];
+  private web3Service: MetamaskService;
   private allTransactionSubscribers = [];
-  public settingsApp = {
-    settings: {
-      checkerDays: 5000,
-      checkerAuctionPool: 5000,
-      checkerStakingInfo: 3600000,
-      checkerBPD: 3600000,
-      freeclaimEndTime: 1611859352000, // Jan-28-2021 06:42:32 PM UTC
 
-      production: false,
-      network: "ropsten",
-      chainsForButtonAddToMetamask: [1, 3, 4],
-      net: 3,
-
-      time: {
-        seconds: 86400,
-        display: "days",
-      },
-    },
-    minutes: {
-      name: "Minutes",
-      shortName: "Min",
-      lowerName: "minutes",
-    },
-    days: {
-      name: "Days",
-      shortName: "Days",
-      lowerName: "days",
-    },
-  };
-
-  private secondsInDay: any;
-  private startDate: any;
-  private leftDays: any;
-
-  private CONTRACTS_PARAMS: any;
-
-  public AxnTokenAddress = "none";
-
-  private dayEndSubscribers: Subscriber<any>[] = [];
-
-  public autoStakeDays: number;
-
-  constructor(private httpService: HttpClient, private config: AppConfig, private contractService: ContractService) {
-    this.web3Service = new MetamaskService(config);
-
-    contractService
-      .accountSubscribe()
-      .subscribe((account: any) => {
-        if (account) {
-          this.isActive = true;
-          this.account = account;
-          this.callAllAccountsSubscribers();
-        }
-      });
-  }
-
-  public getMSecondsInDay() {
-    return this.secondsInDay * 1000;
-  }
-
-  public onDayEnd() {
-    return new Observable((observer) => {
-      this.dayEndSubscribers.push(observer);
-      return {
-        unsubscribe: () => {
-          this.dayEndSubscribers = this.dayEndSubscribers.filter((obs) => {
-            return obs !== observer;
-          });
-        },
-      };
-    });
-  }
-
-  public addToken() {
-    this.web3Service.addToken({
-      address: this.CONTRACTS_PARAMS.HEX2X.ADDRESS,
-      decimals: this.tokensDecimals.HEX2X,
-      image:
-        "https://stake.axion.network/assets/images/icons/axion-icon.png",
-      symbol: "AXN",
+  constructor(private config: AppConfig, private contractService: ContractService) {
+    this.web3Service = new MetamaskService(this.config);
+    this.contractService.accountSubscribe().subscribe((account: any) => {
+      if (account) {
+        this.isActive = true;
+        this.account = account;
+        this.callAllAccountsSubscribers();
+      }
     });
   }
 
@@ -116,21 +34,48 @@ export class MiningContractService {
       });
     }
   }
+
   private callAllTransactionsSubscribers(transaction) {
     this.allTransactionSubscribers.forEach((observer) => {
       observer.next(transaction);
     });
   }
 
+  private checkTransaction(tx) {
+    return new Promise((resolve, reject) => {
+      this.checkTx(tx, resolve, reject);
+    });
+  }
+
+  private initializeContracts() {
+    // this.H2TContract = this.web3Service.getContract(this.CONTRACTS_PARAMS.H2T.ABI, this.CONTRACTS_PARAMS.H2T.ADDRESS);
+    this.axnTokenAddress = this.contractService.CONTRACTS_PARAMS.HEX.ADDRESS;
+  }
+
+  private checkTx(tx, resolve, reject) {
+    this.web3Service.Web3.eth.getTransaction(tx.transactionHash).then((txInfo) => {
+      if (txInfo.blockNumber) {
+        this.callAllTransactionsSubscribers(txInfo);
+        resolve(tx);
+      } else {
+        setTimeout(() => {
+          this.checkTx(tx, resolve, reject);
+        }, 2000);
+      }
+    }, reject);
+  }
+
+  //////////////////
+  // PUBLIC METHODS
+  //////////////////
+
   public accountSubscribe() {
     const newObserver = new Observable((observer) => {
       observer.next(this.account);
       this.allAccountSubscribers.push(observer);
       return {
-        unsubscribe: () => {
-          this.allAccountSubscribers = this.allAccountSubscribers.filter(
-            (a) => a !== newObserver
-          );
+        unsubscribe: () => { 
+          this.allAccountSubscribers = this.allAccountSubscribers.filter(a => a !== newObserver) 
         },
       };
     });
@@ -144,62 +89,9 @@ export class MiningContractService {
     return newObserver;
   }
 
-  public updateETHBalance(callEmitter?) {
-    return new Promise((resolve, reject) => {
-      if (!(this.account && this.account.address)) {
-        return reject();
-      }
-      return this.web3Service
-        .getBalance(this.account.address)
-        .then((balance) => {
-          const bigBalance = new BigNumber(balance);
-          this.account.balances = this.account.balances || {};
-          this.account.balances.ETH = {
-            wei: balance,
-            weiBigNumber: bigBalance,
-            shortBigNumber: bigBalance.div(new BigNumber(10).pow(18)),
-            display: bigBalance.div(new BigNumber(10).pow(18)).toFormat(4),
-          };
-          resolve(null);
-          if (callEmitter) {
-            this.callAllAccountsSubscribers();
-          }
-        });
-    });
-  }
-
-  public getCoinsDecimals() {
-    return this.tokensDecimals;
-  }
-
-  private checkTx(tx, resolve, reject) {
-    this.web3Service.Web3.eth
-      .getTransaction(tx.transactionHash)
-      .then((txInfo) => {
-        if (txInfo.blockNumber) {
-          this.callAllTransactionsSubscribers(txInfo);
-          resolve(tx);
-        } else {
-          setTimeout(() => {
-            this.checkTx(tx, resolve, reject);
-          }, 2000);
-        }
-      }, reject);
-  }
-
-  private checkTransaction(tx) {
-    return new Promise((resolve, reject) => {
-      this.checkTx(tx, resolve, reject);
-    });
-  }
-
-  private initializeContracts() {
-    // this.H2TContract = this.web3Service.getContract(
-    //   this.CONTRACTS_PARAMS.H2T.ABI,
-    //   this.CONTRACTS_PARAMS.H2T.ADDRESS
-    // );
-
-    this.AxnTokenAddress = this.CONTRACTS_PARAMS.HEX.ADDRESS;
+  // TODO: implement this.
+  public async isManager(address) {
+    return address !== "";
   }
 
   public getDecimals(lpTokenAddress): any {
@@ -216,7 +108,6 @@ export class MiningContractService {
         
         const bigBalance = new BigNumber(balance);
         resolve({
-          decimals: decimals,
           wei: balance,
           weiBigNumber: bigBalance,
           shortBigNumber: bigBalance.div(new BigNumber(10).pow(decimals)),
@@ -241,6 +132,4 @@ export class MiningContractService {
       resolve(null)
     })
   }
-
-
 }
